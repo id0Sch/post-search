2026e9d3e882db6c7099ed441c959d3a004bcaa4
Jordan Killpack <jordan@bignerdranch.com>
2014-06-30 22:54:08 -0400

    fix selectors + styling (finally)
    i can't bear to see how randos feel about the scotus ruling any
    longer!

--- shutupguy.js
+++ shutupguy.js
@@ -1,19 +1,22 @@
 var lock = false; // so we don't trigger shutUp() as we manipulate the dom in shutUp()
+
+var facebookStoryClass = ".userContentWrapper";
 var shutUp = function(regex) {
     if (lock) {
         return;
     }
     lock = true;
-    jQuery(".uiUnifiedStory:not(.shutupguy)")
-        .filter(function() { 
-            if (jQuery(this).closest(".uiUnifiedStory.shutupguy").length > 0) {
-                return false; // don't add stuff more than once per story 
+    jQuery(facebookStoryClass + ":not(.shutupguy)")
+        .filter(function() {
+            if (jQuery(this).closest(facebookStoryClass + ".shutupguy").length > 0) {
+                return false; // don't add stuff more than once per story
             }
             var matches = regex.exec(this.textContent);
             if (matches !== null) {
                 var matchingString = matches.join(", ");
-                var story = jQuery(this);/* .closest(".uiUnifiedStory"); */
+                var story = jQuery(this);
                 story.addClass("shutupguy");
+                story.parent().addClass("shutupguy");
 
                 // insert the list of matched words
                 var div = jQuery("<div></div>")
@@ -23,8 +26,8 @@ var shutUp = function(regex) {
                 div.appendTo(jQuery(this));
                 div.css("top", -1 * story.outerHeight() / 2.0);
                 div.css("left", (story.outerWidth() / 2.0) - div.width());
-                
-                return true; 
+
+                return true;
             }
             return false;
         })
@@ -42,7 +45,7 @@ function makeRegex(blacklist) {
 function escape(str) {
     // source: http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711
     return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-} 
+}
 
 chrome.storage.sync.get("shutupguy_blacklist", function(response) {
     var blacklist = response["shutupguy_blacklist"];
@@ -57,7 +60,7 @@ chrome.storage.sync.get("shutupguy_blacklist", function(response) {
         document.addEventListener("DOMNodeInserted", function() {
             // this runs pretty slow-- it'd be better to hook into whatever event Facebook
             // uses to trigger loading additional content, I think.
-            shutUp(regex);    
+            shutUp(regex);
         });
         shutUp(regex);
     }
